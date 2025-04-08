ceb84bcb8e0a94de6dc6fc949df611a3d20820b9
Whizz75 <phokantsekhe@gmail.com>
2025-04-08 20:37:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Replace these values with your actual Render database credentials
 conn = psycopg2.connect(
     dbname="whizz75",  # Use the correct name as seen on Render
-    user="whizz75_user",    # e.g., render_user_xxxxx
+    user="whizz75_user",    # e.g., render_user_xxxx
     password="0z1ICrtCiBCKxJzOL2QKrC7xsBfuik5u",
     host="dpg-cvqi5g6uk2gs73d6ikn0-a",    # e.g., dpg-xxxxxx.databases.render.com
     port="5432",                   # Usually 5432 for PostgreSQL
