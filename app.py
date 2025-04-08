2f12a0959d754fa18d352f41659ba74135294824
Whizz75 <phokantsekhe@gmail.com>
2025-04-08 20:33:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Replace these values with your actual Render database credentials
 conn = psycopg2.connect(
-    dbname="motz_auto_and_tyres",  # Use the correct name as seen on Render
+    dbname="whizz75",  # Use the correct name as seen on Render
     user="whizz75_user",    # e.g., render_user_xxxxx
     password="0z1ICrtCiBCKxJzOL2QKrC7xsBfuik5u",
     host="dpg-cvqi5g6uk2gs73d6ikn0-a",    # e.g., dpg-xxxxxx.databases.render.com
