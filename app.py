eab1a1fe2270a7007b7a6b24d9bbfaddb3ef8095
Whizz75 <phokantsekhe@gmail.com>
2025-04-08 22:03:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 import psycopg2
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Replace these values with your actual Render database credentials
 conn = psycopg2.connect(
