9ba4dce4fdbc7db0bc3546dc2e29a6a6051c43da
Whizz75 <phokantsekhe@gmail.com>
2025-05-03 03:19:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import psycopg2
 
 app = Flask(__name__)
+CORS(app)
 
 # Establish the database connection
 conn = psycopg2.connect(
