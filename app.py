98a3bfdba9f953ccc9b6950b42a16c97718e24c6
Whizz75 <phokantsekhe@gmail.com>
2025-05-13 12:22:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask_cors import CORS
 import psycopg2
 
 app = Flask(__name__)
-CORS(app)
+CORS(app) #Interact with the frontend
 
+#Connect to Hosted DataBase
 conn = psycopg2.connect(
     dbname="semifinal",
     user="semifinal_user",
